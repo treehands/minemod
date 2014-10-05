9cee5af2d78e55ec907f552cb9a9d95a13db1a67
treehands <treehands@users.noreply.github.com>
2014-10-05 17:36:55 +0200

    Error in CommonProxy.java

--- src/main/java/btomoduno/CommonProxy.java
+++ src/main/java/btomoduno/CommonProxy.java
@@ -1,14 +1,9 @@
 package btomoduno;
 
-import blocks.TileEntitySmasher;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 	public void registerRenderThings() {}
 	public void registerSounds() {}
-    public void registerTileEntities() {
-
-        GameRegistry.registerTileEntity(TileEntitySmasher.class, TileEntitySmasher.publicName);
-        
-    }
+    
 }
